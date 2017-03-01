e55737b8b46472b021158fb6ccdb251599680792
Zaid Ur-Rehman <zaidurrehman1@gmail.com>
2017-03-01 08:03:15 +0100

    hovering functionality

--- js/dotplot.js
+++ js/dotplot.js
@@ -6,91 +6,7 @@
  */
 //Note to self, keep everything simple
 
-function hover_cell_function(p) {
-    //todo:calls hovertooltip(i,j) and calls col_header_highlight(i,j) which also gets the arcs for subsequences
-    var par = "#" + this.parentElement.parentElement.parentElement.parentElement.id; // get svg name
-    // console.log(this.parentElement.parentElement.parentElement.parentElement.id);
-    var tooltip = d3.select(this.parentElement.parentElement.parentElement.parentElement.id).append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0)
-        .style("position", "absolute")
-        .style("text-align", "center")
-        .style("width", "60px")
-        .style("height","28px")
-        .style("padding", "2px")
-        .style("font","12px sans-serif")
-        .style("background", "lightsteelblue")
-        .style("border", "0px")
-        .style("border-radius", "8px")
-        .style("pointer-events", "none");
-
-    tooltip.transition()
-            .duration(200)
-            .style("opacity", .9);
-    tooltip.html("hello!")
-        // if ((d.z).toFixed(4) > 0.0000){
-        //     tooltip.html("(" + (d.x + 1) +  ", " + (d.y + 1) + ")<br/>" + (d.z).toFixed(4))
-        //         .style("left", (d3.event.pageX) + "px")
-        //         .style("top", (d3.event.pageY - 28) + "px");
-        // }
-
-
-    // var rowText = par + ' .rowtext';
-    // var colText = par + ' .coltext';
-    // var rect    = par + ' .row .dp_cell rect';
-    // var text    = par + ' .row .dp_cell text';
-    //
-    // d3.selectAll(rowText).classed("active", function(d, i) { return i == p.y; });
-    // d3.selectAll(colText).classed("active", function(d, i) { return i == p.x; });
-    // d3.selectAll(rect).classed("active", function(d, i) { return d.y == p.y && d.x == p.x; });
-    // d3.selectAll(text).attr("display",function(d, i) { if (d.y == p.y && d.x == p.x) return "true"; else return "none" })
-}
-
-function mouseout() {
-    var par = "#" + this.parentElement.parentElement.parentElement.parentElement.id;
-    var rect    = par + ' .row .dp_cell rect';
-
-    d3.selectAll("text").classed("active", false);
-    d3.selectAll(rect).classed("active", false);
-    d3.selectAll(".row .dp_cell text").attr("display","none");
-}
-
-function select_cell_function() {
-    // var par = "#" + this.parentElement.parentElement.parentElement.parentElement.id; // get svg name
-    // var rowText = par + ' .rowtext';
-    // var colText = par + ' .coltext';
-    // var rect    = par + ' .row .dp_cell rect';
-    // var text    = par + ' .row .dp_cell text';
-    //
-    // d3.selectAll(rowText).classed("active", function(d, i) { return i == p.y; });
-    // d3.selectAll(colText).classed("active", function(d, i) { return i == p.x; });
-    // d3.selectAll(rect).classed("active", function(d, i) { return d.y == p.y && d.x == p.x; });
-    // d3.selectAll(text).attr("display",function(d, i) { if (d.y == p.y && d.x == p.x) return "true"; else return "none" });
-}
-
-function showtext() {
-    var par = "#" + this.parentElement.parentElement.parentElement.parentElement.id;
-    var text    = par + ' .row .dp_cell text';
-    d3.selectAll(text)
-        .attr("display","true")
-}
-
-function hidetext() {
-    var par = "#" + this.parentElement.parentElement.parentElement.parentElement.id;
-    var text    = par + ' .row .dp_cell text';
-    d3.selectAll(text)
-        .attr("display","none")
-}
-
-function doubleclick() {
-    var par = "#" + this.parentElement.parentElement.parentElement.parentElement.id;
-    var svg = par + '_svg';
-    d3.selectAll(svg)
-        .attr("transform", "scale(1)");
-}
-
 function dotplot(sequence, table, pname) {
-    // console.log('hi', sequence, table, pname);
 
     // validate input sequence
     for (var i=0; i<sequence.length; i++){
@@ -136,6 +52,7 @@ function dotplot(sequence, table, pname) {
     maindic["base-pairing-probabilities"] = bpp;
     maindic["optimal-structure"] = mlp;
 
+    // resize parent div to quadratic dimensions
     var parent_width = $("#"+pname).width();
     var parent_height = $("#"+pname).height();
     var svg_dim = Math.min(parent_width, parent_height);
@@ -146,22 +63,15 @@ function dotplot(sequence, table, pname) {
 
 
     var bpm=maindic;
-    var cell_length = 50;
     var width = svg_dim,
         height = svg_dim;
     var x = d3.scale.ordinal().rangeBands([0, width]),
-        z = d3.scale.linear().domain([0, 1]).clamp(true),
-        c = d3.scale.category10().domain(d3.range(10));
+        z = d3.scale.linear().domain([0, 1]).clamp(true);
+        // c = d3.scale.category10().domain(d3.range(10));
 
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(30)
-        .size([width, height]);
 
-    /*
-    Zoom functionality code
-     */
-    var zoom = d3.behavior.zoom()       //double click for re-zooming
+    // Zoom functionality code
+    var zoom = d3.behavior.zoom()
         .scaleExtent([1, 10])
         .on("zoom", zoomed);
 
@@ -188,17 +98,9 @@ function dotplot(sequence, table, pname) {
         d3.select(this).classed("dragging", false);
     }
 
+    // draw border of parent div
     var dev = document.getElementById(pname);
     d3.select(dev).style("border", "1px solid black");
-
-    //var svg = d3.select("#output").append("svg")
-    var dev = document.createElement("div"); //todo: remove this div,
-    //todo: re-size the parent div to quadratic, DONE
-    // todo: skip translation DONE
-    d3.select(dev)
-        .style("width", parent_width+"px")
-        .style("height", parent_height+"px");
-        // .style("border", "1px solid black");
 
     //hover information
     var tooltip = d3.select(dev).append("div")
@@ -225,7 +127,7 @@ function dotplot(sequence, table, pname) {
         .call(zoom)
         .on("dblclick.zoom", function(d) {
             svg.attr("transform", "scale(1)")
-        }); //todo: try to define it as a function
+        });
 
     var borderPath = svg.append("rect")
         .attr("x", 0)
@@ -250,6 +152,7 @@ function dotplot(sequence, table, pname) {
     });
 
 
+    //todo: remove rotation of letters
     var column = svg.selectAll(".column")
         .data(matrix)
         .enter().append("g")
@@ -259,7 +162,7 @@ function dotplot(sequence, table, pname) {
     column.append("line")
         .attr("x1", -width);
 
-    column.append("text") //todo: remove rotation of letters
+    column.append("text")
         .attr("class","coltext")
         .attr("x", 6)
         .attr("y", x.rangeBand() / 2)
@@ -267,6 +170,22 @@ function dotplot(sequence, table, pname) {
         .attr("text-anchor", "start")
         .style("font-size", "140%")
         .text(function(d, i) { return isequence[i]; });
+
+
+    //creates blank rects over svg
+    // column.append("rect")
+    //     .attr("x", function(d) { return x(d.x) ; })
+    //     // .attr("x", -6)
+    //     .attr("y", x.rangeBand() / 2)
+    //     .attr("width", x.rangeBand())
+    //     .attr("height",x.rangeBand())
+    //     .attr("border", "1px solid")
+    //     .style("stroke", "lightgrey")
+    //     .style("stroke-width", 1)
+    //     .style("fill", "white" )
+    //     .style("fill-opacity", 1 )
+    //     .text(function(d, i) { return isequence[i]; });
+
 
     var row = svg.selectAll(".row")
         .data(matrix)
@@ -287,15 +206,34 @@ function dotplot(sequence, table, pname) {
         .style("font-size", "140%")
         .text(function(d, i) { return isequence[i]; });
 
+    function hover_cell_function(d) {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        if ((d.z).toFixed(4) > 0.0000){
+            tooltip.html("(" + (d.x + 1) +  ", " + (d.y + 1) + ")<br/>" + (d.z).toFixed(4))
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        }
+    };
+
+    function mouseout(){
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", 0);
+    };
+
+    function select_cell_function() {
+    };
+
     function row(row) {
         var cell = d3.select(this).selectAll(".cell")
             .data(row.filter(function(d) { return d.z; }))
             .enter().append("g")
             .attr("class", "dp_cell");
 
-        cell.append("rect") //todo: mouseover for non-zero
+        cell.append("rect")
             .attr("x", function(d) { return x(d.x) ; })
-            //.attr("cy", 26)
             .attr("width", x.rangeBand())
             .attr("height",x.rangeBand())
             .attr("border", "1px solid")
@@ -303,25 +241,27 @@ function dotplot(sequence, table, pname) {
             .style("stroke-width", 1)
             .style("fill", "white" )
             .style("fill-opacity", 1 )
-            //.style("fill-opacity", function(d) { return Math.pow(z(d.z), 0.5); })
-            // .on("mouseover", hover_cell_function)       //hover function which gets the string
-            // .on("mouseout", mouseout)
+            // .style("fill-opacity", function(d) { return Math.pow(z(d.z), 0.5); })
+            .on("mouseover", hover_cell_function)       //hover function which gets the string
+            .on("mouseout", mouseout)
 
-            .on("mouseover", function(d) {
-                tooltip.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                if ((d.z).toFixed(4) > 0.0000){
-                    tooltip.html("(" + (d.x + 1) +  ", " + (d.y + 1) + ")<br/>" + (d.z).toFixed(4))
-                        .style("left", (d3.event.pageX) + "px")
-                        .style("top", (d3.event.pageY - 28) + "px");
-                }
-            })
-            .on("mouseout", function(d) {
-                tooltip.transition()
-                    .duration(200)
-                    .style("opacity", 0);
-            })
+            // .on("mouseover", function(d) {
+            //     tooltip.transition()
+            //         .duration(200)
+            //         .style("opacity", .9);
+            //     if ((d.z).toFixed(4) > 0.0000){
+            //         tooltip.html("(" + (d.x + 1) +  ", " + (d.y + 1) + ")<br/>" + (d.z).toFixed(4))
+            //             .style("left", (d3.event.pageX) + "px")
+            //             .style("top", (d3.event.pageY - 28) + "px");
+            //     }
+            // })
+            // .on("mouseout", function(d) {
+            //     tooltip.transition()
+            //         .duration(200)
+            //         .style("opacity", 0);
+            // })
+            // .on("mouseover", hover_cell)
+            // .on("mouseout", mouseOUT)
             .on("mouseclick", select_cell_function);
 
         cell.append("circle")
@@ -330,30 +270,30 @@ function dotplot(sequence, table, pname) {
             .attr("r", function(d) { return 0.9*x.rangeBand() * Math.pow(z(d.z), 0.5)/2;})
             .style("stroke-width", 2)
             .style("fill-opacity", function(d) { return Math.pow(z(d.z), 0.5); })
-            // .on("mouseover", hover_cell_function)
-            // .on("mouseout", mouseout)
+            .on("mouseover", hover_cell_function)
+            .on("mouseout", mouseout)
 
-            .on("mouseover", function(d) {
-                tooltip.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                if ((d.z).toFixed(4) > 0.0000){
-                    tooltip.html("(" + (d.x + 1) +  ", " + (d.y + 1) + ")<br/>" + (d.z).toFixed(4))
-                        .style("left", (d3.event.pageX) + "px")
-                        .style("top", (d3.event.pageY - 28) + "px");
-                }
-            })
+            // .on("mouseover", function(d) {
+            //     tooltip.transition()
+            //         .duration(200)
+            //         .style("opacity", .9);
+            //     if ((d.z).toFixed(4) > 0.0000){
+            //         tooltip.html("(" + (d.x + 1) +  ", " + (d.y + 1) + ")<br/>" + (d.z).toFixed(4))
+            //             .style("left", (d3.event.pageX) + "px")
+            //             .style("top", (d3.event.pageY - 28) + "px");
+            //     }
+            // })
 
-            .on("mouseout", function(d) {
-                tooltip.transition()
-                    .duration(200)
-                    .style("opacity", 0);
-            })
+            // .on("mouseout", function(d) {
+            //     tooltip.transition()
+            //         .duration(200)
+            //         .style("opacity", 0);
+            // })
 
+            // .on("mouseover", hover_cell)
+            // .on("mouseout", mouseOUT)
             .on("mouseclick", select_cell_function);
     }
-    return dev;
-    // return d3.select(pname);
 }
 
 
