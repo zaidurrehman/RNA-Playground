943036723d09f0bf0fbd41180314c037382cd920
Zaid Ur-Rehman <zaidurrehman1@gmail.com>
2017-03-07 17:07:01 +0100

    sequence headers, onHover and onClick functions

--- js/dotplot.js
+++ js/dotplot.js
@@ -1,12 +1,89 @@
 /**
- * Created by zr on 27.01.17.
+ * Created by zr in January, 2017.
  */
 /*
  #######################--- Methods for Generating DotPlots ---############################
  */
 //Note to self, keep everything simple
 
-function dotplot(sequence, table, pname) {
+var matrix = []; //todo: matrix should be accessible globally:DONE
+var tooltip;    // for hover information on cells
+var rect_dim;
+
+function renderarc(x, y) {
+    d3.select("small_svg").append("line")
+        .attr("x1", toString(rect_dim/2 + rect_dim * x))
+        .attr("y1", toString(rect_dim/2))
+        .attr("x2", toString(rect_dim/2 + rect_dim * y))
+        .attr("y2", toString(rect_dim/2))
+        .attr("stroke-width", "2");
+}
+
+function hovertext(x, y, z){ //todo: this stays outside as default reaction
+    if (y !== 0 && x !== 0) {
+        if (z.toFixed(4) > 0.0000){
+            tooltip.html("(" + x + ", " + y + ")<br/>" + z.toFixed(4))
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        }
+    }
+}
+
+function clicktext(x, y, z){
+    if (y !== 0 && x !== 0) {
+        renderarc(x, y);
+        tooltip.html("(" + x + ", " + y + ")<br/>" + z.toFixed(4))
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+    }
+
+}
+
+function onHover(d) {  //todo: calls an external func(x,y,z) which tells what to render [null, text]
+    tooltip.style("position", "absolute")
+        .style("text-align", "center")
+        .style("width", "60px")
+        .style("height", "28px")
+        .style("padding", "2px")
+        .style("font", "12px sans-serif")
+        .style("background", "lightsteelblue")
+        .style("border", "0px")
+        .style("border-radius", "8px")
+        .style("pointer-events", "none");
+    tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);    //todo: if d.z is numeric
+    hovertext(d.x, d.y, d.z);
+}
+
+function mouseout(){
+    tooltip.transition()
+        .duration(200)
+        .style("opacity", 0);
+}
+
+function onClick(d) { //todo: this goes inside dotplot function too
+    tooltip.style("opacity", 0)
+        .style("position", "absolute")
+        .style("text-align", "center")
+        .style("width", "60px")
+        .style("height","28px")
+        .style("padding", "2px")
+        .style("font","12px sans-serif")
+        .style("background", "red")
+        .style("border", "0px")
+        .style("border-radius", "16px")
+        .style("pointer-events", "none");
+    tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);
+    // tooltip.html("(" + (d.x) +  ", " + (d.y) + ")<br/>" + (d.z).toFixed(4))
+    //     .style("left", (d3.event.pageX) + "px")
+    //     .style("top", (d3.event.pageY - 28) + "px");
+    clicktext(d.x, d.y, d.z);
+}
+
+function dotplot(sequence, table, pname) {  //todo: hover reaction and click reaction as inputs, both can be null, rename hovertext to hover reaction   
 
     // validate input sequence
     for (var i=0; i<sequence.length; i++){
@@ -18,7 +95,7 @@ function dotplot(sequence, table, pname) {
 
     // validate table size
     if(table.length -1 != sequence.length || table[0].length -1 != sequence.length){
-        alert("Invalid matrix dimensions")
+        alert("Invalid matrix dimensions");
         return "<p>Invalid table entries</p>"
     }
     var maindic = {};
@@ -67,7 +144,6 @@ function dotplot(sequence, table, pname) {
         z = d3.scale.linear().domain([0, 1]).clamp(true);
         // c = d3.scale.category10().domain(d3.range(10));
 
-
     // Zoom functionality code
     var zoom = d3.behavior.zoom()
         .scaleExtent([1, 10])
@@ -101,10 +177,12 @@ function dotplot(sequence, table, pname) {
     d3.select(dev).style("border", "1px solid black");
 
     //hover information
-    var tooltip = d3.select(dev).append("div")
+    // var tooltip = d3.select(dev).append("div") //todo: DONE: this goes outside the dotplot func
+    //     .attr("class", "tooltip");
+    tooltip = d3.select(dev).append("div") //todo: DONE: this goes outside the dotplot func
         .attr("class", "tooltip");
 
-    var svg = d3.select(dev).append("svg")  //todo: put row/col letters in rects, which have id's, can be accessed
+    var svg = d3.select(dev).append("svg")  //todo: put row/col letters in rects, which have id's, can be accessed: DONEf
         .attr("id", pname+"_svg")
         .attr("width", width)
         .attr("height", height)
@@ -116,61 +194,30 @@ function dotplot(sequence, table, pname) {
             svg.attr("transform", "scale(1)")
         });
 
-    var matrix = [];
-
     var seq = bpm["sequence"];
     var isequence = bpm["sequence"];
-    var n = seq.length;
+    var n = seq.length + 1;
     isequence=seq.split("");
 
     x.domain(d3.range(n));
-    isequence.forEach(function(base, i) {
-        base.index = i;
-        matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0}; });
-    });
+    [null].concat(isequence).forEach(function(base, i) {
+        // base.index = i;
+        matrix[i] = d3.range(n).map(function(j) {
+            if(j===0 && i!==0){
+                return {x: j, y:i, z:isequence[i-1]};
+            } else if(i===0 && j!==0) {
+                return {x: j, y: i, z: isequence[j-1]};
+            } else if(i===0 && j===0) {
+                return {x:j, y:i, z:null};
+            } else {
+                return {x: j, y: i, z: 0};
+            }});
+        });
     bpm["base-pairing-probabilities"].forEach(function(link) {
-        matrix[link.source-1][link.target-1].z = link.value;
+        matrix[link.source][link.target].z = link.value;
     });
 
-
-    //todo: remove rotation of letters
-    var column = svg.selectAll(".column")
-        .data(matrix)
-        .enter().append("g")
-        .attr("class", "column")
-        .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
-
-    // column.append("line")
-    //     .attr("x1", -width);
-
-    // column.append("text")
-    //     .attr("class","coltext")
-    //     .attr("x", 6)
-    //     .attr("y", x.rangeBand() / 2)
-    //     .attr("dy", ".32em")
-    //     .attr("text-anchor", "start")
-    //     .style("font-size", "140%")
-    //     .text(function(d, i) { return isequence[i]; });
-
-
-    //creates blank rects over svg
-    column.append("rect")
-        .attr("x", function(d) {return x(d.x) ; })
-        // .attr("x", -6)
-        .attr("y", x.rangeBand() / 2)
-        .attr("width", x.rangeBand())
-        .attr("height",x.rangeBand())
-        .attr("border", "1px solid")
-        .style("stroke", "lightgrey")
-        .style("stroke-width", 1)
-        .style("fill", "white" )
-        .style("fill-opacity", 1 )
-        .append("text")
-        .attr("dy", ".32em")
-        .attr("text-anchor", "start")
-        .style("font-size", "140%")
-        .text(function(d, i) { return isequence[i]; });
-
+    rect_dim = x.rangeBand();
 
     var row = svg.selectAll(".row")
         .data(matrix)
@@ -178,63 +225,6 @@ function dotplot(sequence, table, pname) {
         .attr("class", "row")
         .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
         .each(row);
-
-    row.append("text")
-        .attr("class","rowtext")
-        .attr("x", -6)
-        .attr("y", x.rangeBand() / 2)
-        .attr("dy", ".32em")
-        .attr("text-anchor", "end")
-        .style("font-size", "140%")
-        .text(function(d, i) { return isequence[i]; });
-
-    function hover_cell_function(d) {
-        tooltip.style("opacity", 0)
-            .style("position", "absolute")
-            .style("text-align", "center")
-            .style("width", "60px")
-            .style("height","28px")
-            .style("padding", "2px")
-            .style("font","12px sans-serif")
-            .style("background", "lightsteelblue")
-            .style("border", "0px")
-            .style("border-radius", "8px")
-            .style("pointer-events", "none");
-        tooltip.transition()
-            .duration(200)
-            .style("opacity", .9);
-        if ((d.z).toFixed(4) > 0.0000){
-            tooltip.html("(" + (d.x + 1) +  ", " + (d.y + 1) + ")<br/>" + (d.z).toFixed(4))
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        }
-    }
-
-    function mouseout(){
-        tooltip.transition()
-            .duration(200)
-            .style("opacity", 0);
-    }
-
-    function select_cell_function(d) {
-        tooltip.style("opacity", 0)
-            .style("position", "absolute")
-            .style("text-align", "center")
-            .style("width", "60px")
-            .style("height","28px")
-            .style("padding", "2px")
-            .style("font","12px sans-serif")
-            .style("background", "red")
-            .style("border", "0px")
-            .style("border-radius", "16px")
-            .style("pointer-events", "none");
-        tooltip.transition()
-            .duration(200)
-            .style("opacity", .9);
-        tooltip.html("(" + (d.x + 1) +  ", " + (d.y + 1) + ")<br/>" + (d.z).toFixed(4))
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-}
 
     function row(row) {
         var cell = d3.select(this).selectAll(".cell")
@@ -246,28 +236,55 @@ function dotplot(sequence, table, pname) {
             .attr("x", function(d) { return x(d.x) ; })
             .attr("width", x.rangeBand())
             .attr("height",x.rangeBand())
-            .attr("border", "1px solid")
-            .style("stroke", "lightgrey")
+            .attr("border", "1px solid") //todo: no border for sequence rects,:DONE
+            .attr("id", function (d) { // todo: cell and border different id's: DONE
+                if(d.x === 0 || d.y === 0) {
+                    return "border_" + d.x + "_" + d.y;
+                } else{
+                    return "cell_" + d.x + "_" + d.y;
+                }
+            })
+            .style("stroke", function(d) {
+                if(d.x === 0 || d.y === 0) {
+                    return null;
+                } else {
+                    return  "lightgrey";
+                }
+            })
             .style("stroke-width", 1)
             .style("fill", "white" )
             .style("fill-opacity", 1 )
             // .style("fill-opacity", function(d) { return Math.pow(z(d.z), 0.5); })
-            .on("mouseover", hover_cell_function)       //hover function which gets the string
+            .on("mouseover", onHover)
             .on("mouseout", mouseout)
-            .on("click", select_cell_function);
+            .on("click", onClick);
 
         cell.append("circle")
             .attr("cx", function(d) { return x(d.x) + x.rangeBand() / 2; })
             .attr("cy", function(d) { return x.rangeBand() / 2;})
-            .attr("r", function(d) { return 0.9*x.rangeBand() * Math.pow(z(d.z), 0.5)/2;})
+            .attr("r", function(d) {
+                if(typeof d.z === 'number'){
+                    return 0.9*x.rangeBand() * Math.pow(z(d.z), 0.5)/2;
+                }
+            })
             .style("stroke-width", 2)
             .style("fill-opacity", function(d) { return Math.pow(z(d.z), 0.5); })
-            .on("mouseover", hover_cell_function)
+            .on("mouseover", onHover)
             .on("mouseout", mouseout)
-            .on("click", select_cell_function);
+            .on("click", onClick);
+
+
+        cell.append("text")
+            .attr("x", function(d) { return x(d.x) + x.rangeBand()/2; })
+            .attr("y", x.rangeBand()/2)
+            .attr("dy", ".32em")
+            .attr("text-anchor", "middle")
+            .style("font-size", "140%")
+            .text(function(d) { if(typeof d.z === 'string'){
+                return d.z;
+            } });
     }
 }
-
 
 function getHybridSequence( seq1, seq2, minLoopLength ) {
     // add first sequence
